4f344e13839443410d1f523892824f94286bede6
CJPALME <cjpalmerin@gmail.com>
2020-05-28 23:51:15 -0500

    complete App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,70 @@
 import React from 'react';
-import Home from './pages/Home'
 import './App.css';
-import Navbar from './components/Navbar';
-import Footer from './components/Footer';
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
+
+import Home from './pages/Home';
+import AboutMe from './pages/AboutMe'
+import Projects from './pages/Projects';
+import Contact from './pages/Contact';
+import Resume from './pages/Resume';
+import WeatherEats from './pages/WeatherEats';
+import LocalSpot from './pages/LocalSpot';
+
 
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-      <Home />
-      <Footer />
-    </div>
+    <Router>
+      <Route exact path="/" render={props => (
+        <React.Fragment>
+          <Home />
+        </React.Fragment>
+      )} />
+      <Route exact path="/aboutme" render={props => (
+        <React.Fragment>
+          <Navbar />
+          <AboutMe />
+          <Footer />
+        </React.Fragment>
+      )} />
+      <Route exact path="/projects" render={props => (
+        <React.Fragment>
+          <Navbar />
+          <Projects />
+          <Footer />
+        </React.Fragment>
+      )} />
+      <Route exact path="/weathereats" render={props => (
+        <React.Fragment>
+          <Navbar />
+          <WeatherEats />
+          <Footer />
+        </React.Fragment>
+      )} />
+      <Route exact path="/localspot" render={props => (
+        <React.Fragment>
+          <Navbar />
+          <LocalSpot />
+          <Footer />
+        </React.Fragment>
+      )} />
+      <Route exact path="/resume" render={props => (
+        <React.Fragment>
+          <Navbar />
+          <Resume />
+          <Footer />
+        </React.Fragment>
+      )} />
+      <Route exact path="/contact" render={props => (
+        <React.Fragment>
+          <Navbar />
+          <Contact />
+          <Footer />
+        </React.Fragment>
+      )} />
+    </Router>
   );
 }
 
