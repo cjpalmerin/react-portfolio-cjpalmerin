1b4159f9222a71ce5c1ab15013293ed161fa3301
CJPALME <cjpalmerin@gmail.com>
2020-05-29 00:20:26 -0500

    added * route to server.js

--- server.js
+++ server.js
@@ -17,6 +17,9 @@ router.use((req, res) => {
   res.sendFile(path.join(__dirname, "../client/build/index.html"))
 });
 
+app.get('*', (req, res) => res.sendFile(path.resolve('build', 'index.html')));
+
+
 // Start the API server
 app.listen(PORT, function() {
   console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
